cb6c1029c6b5a74951b171452113ff9fb0e0ac9d
Ki-sauce <itiskrishnagupta@gmail.com>
2025-07-01 16:42:33 +0530

    Update firebase.js
    removed public api key

--- everleaf-frontend/src/services/firebase.js
+++ everleaf-frontend/src/services/firebase.js
@@ -1,21 +1,24 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCf48K-TS32ZL047_iLGFrGNRbjvAnrLAY",
-  authDomain: "everleaf-30e5d.firebaseapp.com",
-  projectId: "everleaf-30e5d",
-  storageBucket: "everleaf-30e5d.firebasestorage.app",
-  messagingSenderId: "452083961396",
-  appId: "1:452083961396:web:1e1810d00d07f93a0c0461",
-  measurementId: "G-3BS5B9R7NK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+isSupported().then((yes) => {
+  if (yes) getAnalytics(app);
+});
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
